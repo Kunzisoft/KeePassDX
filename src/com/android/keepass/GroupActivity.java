fc1c2199dacaf938146406e210ad1cd54a812da8
Brian Pellin <bpellin@gmail.com>
2009-06-27 15:43:33 -0500

    Refactor into two classes

--- src/com/android/keepass/GroupActivity.java
+++ src/com/android/keepass/GroupActivity.java
@@ -23,58 +23,9 @@ import java.lang.ref.WeakReference;
 
 import org.phoneid.keepassj2me.PwGroup;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-public class GroupActivity extends LockingListActivity {
-
-	public static final String KEY_ENTRY = "entry";
-	
-	protected static final int MENU_LOCK = Menu.FIRST;
-	
-	protected PwGroup mGroup;
-
-	public static void Launch(Activity act, PwGroup group) {
-		Intent i = new Intent(act, GroupActivity.class);
-		
-		if ( group != null ) {
-			i.putExtra(KEY_ENTRY, group.groupId);
-		}
-		
-		act.startActivityForResult(i,0);
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		
-		if ( KeePass.db.gDirty.get(mGroup) != null ) {
-			KeePass.db.gDirty.remove(mGroup);
-			BaseAdapter adapter = (BaseAdapter) getListAdapter();
-			adapter.notifyDataSetChanged();
-			
-		}
-	}
-
-	private int mId;
-	
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-
-		ListAdapter adapt = getListAdapter();
-		ClickView cv = (ClickView) adapt.getView(position, null, null);
-		cv.onClick();
-		
-	}
+public class GroupActivity extends GroupBaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +34,6 @@ public class GroupActivity extends LockingListActivity {
 		setResult(KeePass.EXIT_NORMAL);
 		
 		int id = getIntent().getIntExtra(KEY_ENTRY, -1);
-		assert(mId >= 0);
 		
 		if ( id == -1 ) {
 			mGroup = KeePass.db.gRoot;
@@ -96,56 +46,5 @@ public class GroupActivity extends LockingListActivity {
 		setGroupTitle();
 		
 		setListAdapter(new PwListAdapter(this, mGroup));
-		styleScrollBars();
-		
-	}
-	
-	protected void styleScrollBars() {
-		ListView lv = getListView();
-		lv.setScrollBarStyle(View.SCROLLBARS_INSIDE_INSET);
-		lv.setTextFilterEnabled(true);
-		
-	}
-	
-	private void setGroupTitle() {
-		if ( mGroup != null ) {
-			String name = mGroup.name;
-			if ( name != null && name.length() > 0 ) {
-				TextView tv = (TextView) findViewById(R.id.group_header);
-				tv.setText(getText(R.string.current_group) + " " + name);
-			}
-		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		
-		menu.add(0, MENU_LOCK, 0, R.string.menu_lock);
-		menu.findItem(MENU_LOCK).setIcon(android.R.drawable.ic_lock_lock);
-		
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch ( item.getItemId() ) {
-		case MENU_LOCK:
-			setResult(KeePass.EXIT_LOCK);
-			finish();
-			return true;
-		}
-		
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		if (resultCode == KeePass.EXIT_LOCK ) {
-			setResult(KeePass.EXIT_LOCK);
-			finish();
-		}
 	}
 }
