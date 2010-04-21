caea325050053dcc4a7526e08fe4888e57e4a8d8
Brian Pellin <bpellin@gmail.com>
2010-04-20 22:00:51 -0500

    Refactor out the group header portion of the view.

---
+++ src/com/keepassdroid/GroupHeaderView.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2009 Brian Pellin.
+ *     
+ * This file is part of KeePassDroid.
+ *
+ *  KeePassDroid is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  KeePassDroid is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KeePassDroid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.keepassdroid;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+import com.android.keepass.R;
+
+public class GroupHeaderView extends RelativeLayout {
+
+	public GroupHeaderView(Context context) {
+		super(context);
+		
+		inflate(context);
+	}
+	
+	public GroupHeaderView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		inflate(context);
+	}
+	
+	private void inflate(Context context) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.group_header, this);
+		
+	}
+
+
+}
