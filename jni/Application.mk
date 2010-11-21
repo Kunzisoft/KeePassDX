731d986fa85b1bae18e3064f99a6800c75df0886
Brian Pellin <bpellin@gmail.com>
2010-11-21 16:11:39 -0600

    Rearrange build tree, since newer ndk's make jni_root unneeded.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,3 @@
-APP_PROJECT_PATH := $(call my-dir)
 APP_MODULES      := aes sha final-key
 APP_OPTIM        := release
 APP_ABI          := armeabi armeabi-v7a
--- jni/prep_build.sh
+++ jni/prep_build.sh
@@ -3,9 +3,7 @@ http://gladman.plushost.co.uk/oldsite/AES/
 AES_FILE="aes-src-29-04-09.zip"
 SHA_FILE="sha2-07-01-07.zip"
 EXTRACT_PATH=project/jni/
-PATCH=build.patch
 
-ln -s ../ project
 curl http://gladman.plushost.co.uk/oldsite/AES/$AES_FILE > $AES_FILE
 unzip $AES_FILE -d $EXTRACT_PATH/aes
 curl http://gladman.plushost.co.uk/oldsite/cryptography_technology/sha/$SHA_FILE > $SHA_FILE
