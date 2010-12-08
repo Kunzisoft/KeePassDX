ede28a5ace6c686aaec8e3c3c688cda5bda7d406
Brian Pellin <bpellin@gmail.com>
2010-12-07 21:15:56 -0600

    Fix paths

--- jni/prep_build.sh
+++ jni/prep_build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-http://gladman.plushost.co.uk/oldsite/AES/
 AES_FILE="aes-src-29-04-09.zip"
 SHA_FILE="sha2-07-01-07.zip"
-EXTRACT_PATH=project/jni/
 
 curl http://gladman.plushost.co.uk/oldsite/AES/$AES_FILE > $AES_FILE
-unzip $AES_FILE -d $EXTRACT_PATH/aes
+unzip $AES_FILE -d aes
 curl http://gladman.plushost.co.uk/oldsite/cryptography_technology/sha/$SHA_FILE > $SHA_FILE
-unzip $SHA_FILE -d $EXTRACT_PATH/sha
+unzip $SHA_FILE -d sha
