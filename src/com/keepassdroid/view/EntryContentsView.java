29ea14034dac82c288cc1fbdbbc16197fabdc4e3
Brian Pellin <bpellin@gmail.com>
2010-12-08 22:11:46 -0600

    Refactor out common view.

---
+++ src/com/keepassdroid/view/EntryContentsView.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2010 Brian Pellin.
+ *     
+ * This file is part of KeePassDroid.
+ *
+ *  KeePassDroid is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  KeePassDroid is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KeePassDroid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.keepassdroid.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.TableLayout;
+
+import com.android.keepass.R;
+
+public class EntryContentsView extends TableLayout {
+	
+	public EntryContentsView(Context context) {
+		super(context);
+		
+		inflate(context);
+	}
+	
+	public EntryContentsView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		inflate(context);
+	}
+	
+	private void inflate(Context context) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.entry_view_contents, this);
+	}
+
+}
