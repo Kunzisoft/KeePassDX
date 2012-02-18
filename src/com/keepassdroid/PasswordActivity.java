9103a4647f93f4a11fde3076498b8fa471d0c7a6
Brian Pellin <bpellin@gmail.com>
2012-02-18 13:29:58 -0700

    Protect against exception.

--- src/com/keepassdroid/PasswordActivity.java
+++ src/com/keepassdroid/PasswordActivity.java
@@ -245,7 +245,9 @@ public class PasswordActivity extends LockingActivity {
 	
 	@Override
 	protected void onDestroy() {
-		unregisterReceiver(mIntentReceiver);
+		if (mIntentReceiver != null) {
+			unregisterReceiver(mIntentReceiver);
+		}
 		
 		super.onDestroy();
 	}
