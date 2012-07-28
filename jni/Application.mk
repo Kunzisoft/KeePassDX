b8ee1f6592c23fddeeb9fe932a6cc20ea1b3c844
Brian Pellin <bpellin@gmail.com>
2012-07-28 14:55:55 -0500

    Fix APP_ABI support for ndk r8b.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,3 @@
 APP_MODULES      := aes sha final-key
 APP_OPTIM        := release
-APP_ABI          := armeabi armeabi-v7a x86 mips mips-r2 mips-r2-sf
+APP_ABI          := armeabi armeabi-v7a x86 mips
