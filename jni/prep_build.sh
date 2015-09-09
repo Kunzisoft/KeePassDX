4813431edadbc0a58485d06879b56250e479af77
Brian Pellin <bpellin@gmail.com>
2015-09-08 22:12:06 -0500

    Update link to sha source.

--- jni/prep_build.sh
+++ jni/prep_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SHA_FILE="sha2-07-01-07.zip"
 
-curl http://brgladman.org/oldsite/cryptography_technology/sha/$SHA_FILE > $SHA_FILE
+curl http://173.254.28.24/~brgladma/oldsite/cryptography_technology/sha/$SHA_FILE > $SHA_FILE
 unzip $SHA_FILE -d sha
