af686e151b8c943283e22018ba7c7cd1f8d0f78b
Brian Pellin <bpellin@gmail.com>
2015-12-08 22:19:10 -0600

    Formatting.

--- src/com/keepassdroid/database/exception/KeyFileEmptyException.java
+++ src/com/keepassdroid/database/exception/KeyFileEmptyException.java
@@ -16,7 +16,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with KeePassDroid.  If not, see <http://www.gnu.org/licenses/>.
  *
- */package com.keepassdroid.database.exception;
+ */
+package com.keepassdroid.database.exception;
 
 public class KeyFileEmptyException extends InvalidKeyFileException {
 	/**
