a454e719f81b015d05e8aada881d3642b075435e
Jeremy <jeremy.jamet@kunzisoft.com>
2017-10-27 18:57:04 +0200

    Change Project Name

--- app/src/main/java/com/keepassdroid/database/load/ImporterV4.java
+++ app/src/main/java/com/keepassdroid/database/load/ImporterV4.java
@@ -1,20 +1,20 @@
 /*
  * Copyright 2017 Brian Pellin, Jeremy Jamet / Kunzisoft.
  *     
- * This file is part of KeePassDroid.
+ * This file is part of KeePass DX.
  *
- *  KeePassDroid is free software: you can redistribute it and/or modify
+ *  KeePass DX is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 2 of the License, or
  *  (at your option) any later version.
  *
- *  KeePassDroid is distributed in the hope that it will be useful,
+ *  KeePass DX is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with KeePassDroid.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with KeePass DX.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 package com.keepassdroid.database.load;
