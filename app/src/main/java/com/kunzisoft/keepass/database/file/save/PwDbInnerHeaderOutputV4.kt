a75d237e538224f94d2a010098387b36b1c59e58
J-Jamet <jeremy.jamet@kunzisoft.com>
2019-09-09 10:11:16 +0200

    Kotlinized MemUtil

--- app/src/main/java/com/kunzisoft/keepass/database/file/save/PwDbInnerHeaderOutputV4.kt
+++ app/src/main/java/com/kunzisoft/keepass/database/file/save/PwDbInnerHeaderOutputV4.kt
@@ -21,6 +21,7 @@ package com.kunzisoft.keepass.database.file.save
 
 import com.kunzisoft.keepass.database.element.PwDatabaseV4
 import com.kunzisoft.keepass.database.file.PwDbHeaderV4
+import com.kunzisoft.keepass.stream.ActionReadBytes
 import com.kunzisoft.keepass.stream.LEDataOutputStream
 import com.kunzisoft.keepass.utils.MemUtil
 import java.io.IOException
@@ -55,7 +56,9 @@ class PwDbInnerHeaderOutputV4(private val db: PwDatabaseV4, private val header: 
             los.write(flag.toInt())
 
             protectedBinary.getData()?.let {
-                MemUtil.readBytes(it) { buffer -> los.write(buffer) }
+                MemUtil.readBytes(it, ActionReadBytes { buffer ->
+                    los.write(buffer)
+                })
             } ?: throw IOException("Can't write protected binary")
         }
 
