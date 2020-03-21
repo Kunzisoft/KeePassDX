81ea7080c293f82a99d4c16492eaf79664d0586b
J-Jamet <jeremy.jamet@kunzisoft.com>
2020-03-21 20:25:37 +0100

    Show UUID as KeePass UUID

--- app/src/main/java/com/kunzisoft/keepass/view/EntryContentsView.kt
+++ app/src/main/java/com/kunzisoft/keepass/view/EntryContentsView.kt
@@ -31,6 +31,7 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
 import com.kunzisoft.keepass.R
 import com.kunzisoft.keepass.adapters.EntryAttachmentsAdapter
 import com.kunzisoft.keepass.adapters.EntryHistoryAdapter
@@ -40,8 +41,8 @@ import com.kunzisoft.keepass.database.element.security.ProtectedString
 import com.kunzisoft.keepass.model.EntryAttachment
 import com.kunzisoft.keepass.otp.OtpElement
 import com.kunzisoft.keepass.otp.OtpType
+import com.kunzisoft.keepass.utils.toKeePassRefString
 import java.util.*
-import androidx.recyclerview.widget.SimpleItemAnimator
 
 
 class EntryContentsView @JvmOverloads constructor(context: Context,
@@ -345,7 +346,7 @@ class EntryContentsView @JvmOverloads constructor(context: Context,
     }
 
     fun assignUUID(uuid: UUID) {
-        uuidView.text = uuid.toString()
+        uuidView.text = uuid.toKeePassRefString()
     }
 
     /* -------------
