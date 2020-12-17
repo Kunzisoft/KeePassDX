e688859e32e4f644db7d1320abba1a4cc4b45245
J-Jamet <jeremy.jamet@kunzisoft.com>
2020-12-17 12:32:30 +0100

    Fix exception when UI not fully loaded and click performed

--- app/src/main/java/com/kunzisoft/keepass/biometric/AdvancedUnlockFragment.kt
+++ app/src/main/java/com/kunzisoft/keepass/biometric/AdvancedUnlockFragment.kt
@@ -237,7 +237,7 @@ class AdvancedUnlockFragment: StylishFragment(), AdvancedUnlockManager.AdvancedU
                             Mode.WAIT_CREDENTIAL
                         })
                     }
-                } ?: throw IODatabaseException()
+                }
             }
         }
     }
