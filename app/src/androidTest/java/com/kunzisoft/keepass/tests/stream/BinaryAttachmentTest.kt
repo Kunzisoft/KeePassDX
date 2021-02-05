c70faaedd17ca2d2b30501aa51e1539b96c56079
J-Jamet <jeremy.jamet@kunzisoft.com>
2021-02-05 13:44:30 +0100

    Rename BinaryAttachment to BinaryAttachmentTest

--- app/src/androidTest/java/com/kunzisoft/keepass/tests/stream/BinaryAttachmentTest.kt
+++ app/src/androidTest/java/com/kunzisoft/keepass/tests/stream/BinaryAttachmentTest.kt
@@ -14,7 +14,7 @@ import java.io.InputStream
 import java.lang.Exception
 import java.security.MessageDigest
 
-class BinaryAttachment {
+class BinaryAttachmentTest {
 
     private val context: Context by lazy {
         InstrumentationRegistry.getInstrumentation().context
