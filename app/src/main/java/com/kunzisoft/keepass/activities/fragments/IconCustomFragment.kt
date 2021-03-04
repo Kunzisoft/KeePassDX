e063b0d6fced77ca2ba0f42ea90c377e3f246890
J-Jamet <jeremy.jamet@kunzisoft.com>
2021-03-04 19:48:13 +0100

    Remove unused code

--- app/src/main/java/com/kunzisoft/keepass/activities/fragments/IconCustomFragment.kt
+++ app/src/main/java/com/kunzisoft/keepass/activities/fragments/IconCustomFragment.kt
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2021 Jeremy Jamet / Kunzisoft.
+ *
+ * This file is part of KeePassDX.
+ *
+ *  KeePassDX is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  KeePassDX is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KeePassDX.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package com.kunzisoft.keepass.activities.fragments
 
 import android.os.Bundle
@@ -7,14 +26,11 @@ import com.kunzisoft.keepass.adapters.IconAdapter
 import com.kunzisoft.keepass.database.element.Database
 import com.kunzisoft.keepass.database.element.icon.IconImageCustom
 
-/**
- * Content fragments
- */
 
 class IconCustomFragment : IconFragment<IconImageCustom>() {
 
     override fun retrieveMainLayoutId(): Int {
-        return R.layout.fragment_icon_custom_picker
+        return R.layout.fragment_icon_grid
     }
 
     override fun defineIconList(database: Database): List<IconImageCustom> {
