cfcfd4770583c8174384111a0fa38075c9764e03
J-Jamet <jeremy.jamet@kunzisoft.com>
2021-03-24 11:53:36 +0100

    Fix test

--- app/src/androidTest/java/com/kunzisoft/keepass/tests/stream/BinaryDataTest.kt
+++ app/src/androidTest/java/com/kunzisoft/keepass/tests/stream/BinaryDataTest.kt
@@ -42,8 +42,8 @@ class BinaryDataTest {
         val binaryB = BinaryFile(fileB)
         saveBinary(TEST_TEXT_ASSET, binaryA)
         saveBinary(TEST_TEXT_ASSET, binaryB)
-        assertEquals("Save text binary length failed.", binaryA.getSize(binaryCache), binaryB.getSize(binaryCache))
-        assertEquals("Save text binary MD5 failed.", binaryA.binaryHash(binaryCache), binaryB.binaryHash(binaryCache))
+        assertEquals("Save text binary length failed.", binaryA.getSize(), binaryB.getSize())
+        assertEquals("Save text binary MD5 failed.", binaryA.binaryHash(), binaryB.binaryHash())
     }
 
     @Test
@@ -52,8 +52,8 @@ class BinaryDataTest {
         val binaryB = BinaryFile(fileB)
         saveBinary(TEST_IMAGE_ASSET, binaryA)
         saveBinary(TEST_IMAGE_ASSET, binaryB)
-        assertEquals("Save image binary length failed.", binaryA.getSize(binaryCache), binaryB.getSize(binaryCache))
-        assertEquals("Save image binary failed.", binaryA.binaryHash(binaryCache), binaryB.binaryHash(binaryCache))
+        assertEquals("Save image binary length failed.", binaryA.getSize(), binaryB.getSize())
+        assertEquals("Save image binary failed.", binaryA.binaryHash(), binaryB.binaryHash())
     }
 
     @Test
@@ -66,11 +66,11 @@ class BinaryDataTest {
         saveBinary(TEST_TEXT_ASSET, binaryC)
         binaryA.compress(binaryCache)
         binaryB.compress(binaryCache)
-        assertEquals("Compress text length failed.", binaryA.getSize(binaryCache), binaryB.getSize(binaryCache))
-        assertEquals("Compress text MD5 failed.", binaryA.binaryHash(binaryCache), binaryB.binaryHash(binaryCache))
+        assertEquals("Compress text length failed.", binaryA.getSize(), binaryB.getSize())
+        assertEquals("Compress text MD5 failed.", binaryA.binaryHash(), binaryB.binaryHash())
         binaryB.decompress(binaryCache)
-        assertEquals("Decompress text length failed.", binaryB.getSize(binaryCache), binaryC.getSize(binaryCache))
-        assertEquals("Decompress text MD5 failed.", binaryB.binaryHash(binaryCache), binaryC.binaryHash(binaryCache))
+        assertEquals("Decompress text length failed.", binaryB.getSize(), binaryC.getSize())
+        assertEquals("Decompress text MD5 failed.", binaryB.binaryHash(), binaryC.binaryHash())
     }
 
     @Test
@@ -83,12 +83,12 @@ class BinaryDataTest {
         saveBinary(TEST_IMAGE_ASSET, binaryC)
         binaryA.compress(binaryCache)
         binaryB.compress(binaryCache)
-        assertEquals("Compress image length failed.", binaryA.getSize(binaryCache), binaryA.getSize(binaryCache))
-        assertEquals("Compress image failed.", binaryA.binaryHash(binaryCache), binaryA.binaryHash(binaryCache))
+        assertEquals("Compress image length failed.", binaryA.getSize(), binaryA.getSize())
+        assertEquals("Compress image failed.", binaryA.binaryHash(), binaryA.binaryHash())
         binaryB = BinaryFile(fileB, true)
         binaryB.decompress(binaryCache)
-        assertEquals("Decompress image length failed.", binaryB.getSize(binaryCache), binaryC.getSize(binaryCache))
-        assertEquals("Decompress image failed.", binaryB.binaryHash(binaryCache), binaryC.binaryHash(binaryCache))
+        assertEquals("Decompress image length failed.", binaryB.getSize(), binaryC.getSize())
+        assertEquals("Decompress image failed.", binaryB.binaryHash(), binaryC.binaryHash())
     }
 
     @Test
@@ -101,12 +101,12 @@ class BinaryDataTest {
         binaryA.compress(binaryCache)
         binaryB.compress(binaryCache)
         assertEquals("Compress bytes decompressed failed.", binaryA.isCompressed, true)
-        assertEquals("Compress bytes length failed.", binaryA.getSize(binaryCache), binaryA.getSize(binaryCache))
-        assertEquals("Compress bytes failed.", binaryA.binaryHash(binaryCache), binaryA.binaryHash(binaryCache))
+        assertEquals("Compress bytes length failed.", binaryA.getSize(), binaryA.getSize())
+        assertEquals("Compress bytes failed.", binaryA.binaryHash(), binaryA.binaryHash())
         binaryB.decompress(binaryCache)
         assertEquals("Decompress bytes decompressed failed.", binaryB.isCompressed, false)
-        assertEquals("Decompress bytes length failed.", binaryB.getSize(binaryCache), binaryC.getSize(binaryCache))
-        assertEquals("Decompress bytes failed.", binaryB.binaryHash(binaryCache), binaryC.binaryHash(binaryCache))
+        assertEquals("Decompress bytes length failed.", binaryB.getSize(), binaryC.getSize())
+        assertEquals("Decompress bytes failed.", binaryB.binaryHash(), binaryC.binaryHash())
     }
 
     @Test
