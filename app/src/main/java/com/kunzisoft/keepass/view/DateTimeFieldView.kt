b4c3f831a775bebe5a5137b93876a1f88f4a5972
J-Jamet <jeremy.jamet@kunzisoft.com>
2021-07-18 14:52:23 +0200

    Set date time from string

--- app/src/main/java/com/kunzisoft/keepass/view/DateTimeFieldView.kt
+++ app/src/main/java/com/kunzisoft/keepass/view/DateTimeFieldView.kt
@@ -136,7 +136,11 @@ class DateTimeFieldView @JvmOverloads constructor(context: Context,
             return if (activation) dateTime.toString() else ""
         }
         set(value) {
-            // TODO set date time string value
+            mDateTime = try {
+                DateInstant(value)
+            } catch (e: Exception) {
+                DateInstant.NEVER_EXPIRES
+            }
         }
 
     override var isFieldVisible: Boolean
