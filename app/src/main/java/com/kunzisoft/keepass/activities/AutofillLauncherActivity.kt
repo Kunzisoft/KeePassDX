483aca871a1a15f9701887929b901ae7eb242b68
J-Jamet <jeremy.jamet@kunzisoft.com>
2021-12-09 13:38:42 +0100

    Upgrade to version 3.0.4 and fix inline autofill suggestion #1165

--- app/src/main/java/com/kunzisoft/keepass/activities/AutofillLauncherActivity.kt
+++ app/src/main/java/com/kunzisoft/keepass/activities/AutofillLauncherActivity.kt
@@ -24,6 +24,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.view.inputmethod.InlineSuggestionsRequest
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -65,17 +66,31 @@ class AutofillLauncherActivity : DatabaseModeActivity() {
         EntrySelectionHelper.retrieveSpecialModeFromIntent(intent).let { specialMode ->
             when (specialMode) {
                 SpecialMode.SELECTION -> {
-                    // Build search param
-                    val searchInfo = SearchInfo().apply {
-                        applicationId = intent.getStringExtra(KEY_SEARCH_APPLICATION_ID)
-                        webDomain = intent.getStringExtra(KEY_SEARCH_DOMAIN)
-                        webScheme = intent.getStringExtra(KEY_SEARCH_SCHEME)
-                        manualSelection = intent.getBooleanExtra(KEY_MANUAL_SELECTION, false)
+                    intent.getBundleExtra(KEY_SELECTION_BUNDLE)?.let { bundle ->
+                        // Build search param
+                        bundle.getParcelable<SearchInfo>(KEY_SEARCH_INFO)?.let { searchInfo ->
+                            SearchInfo.getConcreteWebDomain(
+                                this,
+                                searchInfo.webDomain
+                            ) { concreteWebDomain ->
+                                searchInfo.webDomain = concreteWebDomain
+                                launchSelection(database, searchInfo)
+                            }
+                        }
+                        // To pass extra inline request
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            bundle.getParcelable<InlineSuggestionsRequest>(
+                                EXTRA_INLINE_SUGGESTIONS_REQUEST
+                            )?.let { inlineSuggestionRequest ->
+                                intent.putExtra(
+                                    EXTRA_INLINE_SUGGESTIONS_REQUEST,
+                                    inlineSuggestionRequest
+                                )
+                            }
+                        }
                     }
-                    SearchInfo.getConcreteWebDomain(this, searchInfo.webDomain) { concreteWebDomain ->
-                        searchInfo.webDomain = concreteWebDomain
-                        launchSelection(database, searchInfo)
-                    }
+                    // Remove bundle
+                    intent.removeExtra(KEY_SELECTION_BUNDLE)
                 }
                 SpecialMode.REGISTRATION -> {
                     // To register info
@@ -195,30 +210,26 @@ class AutofillLauncherActivity : DatabaseModeActivity() {
 
     companion object {
 
-        private const val KEY_MANUAL_SELECTION = "KEY_MANUAL_SELECTION"
-        private const val KEY_SEARCH_APPLICATION_ID = "KEY_SEARCH_APPLICATION_ID"
-        private const val KEY_SEARCH_DOMAIN = "KEY_SEARCH_DOMAIN"
-        private const val KEY_SEARCH_SCHEME = "KEY_SEARCH_SCHEME"
+        private const val KEY_SELECTION_BUNDLE = "KEY_SELECTION_BUNDLE"
+        private const val KEY_SEARCH_INFO = "KEY_SEARCH_INFO"
 
         private const val KEY_REGISTER_INFO = "KEY_REGISTER_INFO"
 
         fun getPendingIntentForSelection(context: Context,
                                          searchInfo: SearchInfo? = null,
-                                         inlineSuggestionsRequest: CompatInlineSuggestionsRequest? = null): PendingIntent {
+                                         compatInlineSuggestionsRequest: CompatInlineSuggestionsRequest? = null): PendingIntent {
             return PendingIntent.getActivity(context, 0,
-                // Doesn't work with Parcelable (don't know why?)
+                // Doesn't work with direct extra Parcelable (don't know why?)
+                // Wrap into a bundle to bypass the problem
                 Intent(context, AutofillLauncherActivity::class.java).apply {
-                    searchInfo?.let {
-                        putExtra(KEY_SEARCH_APPLICATION_ID, it.applicationId)
-                        putExtra(KEY_SEARCH_DOMAIN, it.webDomain)
-                        putExtra(KEY_SEARCH_SCHEME, it.webScheme)
-                        putExtra(KEY_MANUAL_SELECTION, it.manualSelection)
-                    }
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                        inlineSuggestionsRequest?.let {
-                            putExtra(EXTRA_INLINE_SUGGESTIONS_REQUEST, it)
+                    putExtra(KEY_SELECTION_BUNDLE, Bundle().apply {
+                        putParcelable(KEY_SEARCH_INFO, searchInfo)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            compatInlineSuggestionsRequest?.inlineSuggestionsRequest?.let {
+                                putParcelable(EXTRA_INLINE_SUGGESTIONS_REQUEST, it)
+                            }
                         }
-                    }
+                    })
                 },
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     // TODO Mutable
