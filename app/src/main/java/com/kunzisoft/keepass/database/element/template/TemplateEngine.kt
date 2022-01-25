82177a386efd41f2f1af7216153ca8a87992a18a
J-Jamet <jeremy.jamet@kunzisoft.com>
2022-01-25 11:24:23 +0100

    Add copyright

--- app/src/main/java/com/kunzisoft/keepass/database/element/template/TemplateEngine.kt
+++ app/src/main/java/com/kunzisoft/keepass/database/element/template/TemplateEngine.kt
@@ -1,5 +1,22 @@
 package com.kunzisoft.keepass.database.element.template
-
+/*
+ * Copyright 2021 Jeremy Jamet / Kunzisoft.
+ *
+ * This file is part of KeePassDX.
+ *
+ *  KeePassDX is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  KeePassDX is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KeePassDX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import android.content.res.Resources
 import android.util.Log
 import com.kunzisoft.keepass.R
