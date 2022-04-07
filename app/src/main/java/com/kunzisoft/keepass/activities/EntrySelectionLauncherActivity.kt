94c02b7288e54c3be48fdcf93613991dfb8d1ba6
J-Jamet <jeremy.jamet@kunzisoft.com>
2022-04-07 13:26:07 +0200

    Fix selection mode instance issue

--- app/src/main/java/com/kunzisoft/keepass/activities/EntrySelectionLauncherActivity.kt
+++ app/src/main/java/com/kunzisoft/keepass/activities/EntrySelectionLauncherActivity.kt
@@ -225,7 +225,6 @@ class EntrySelectionLauncherActivity : DatabaseModeActivity() {
             }
             // New task needed because don't launch from an Activity context
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                    Intent.FLAG_ACTIVITY_CLEAR_TASK or
                     Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
             context.startActivity(intent)
         }
