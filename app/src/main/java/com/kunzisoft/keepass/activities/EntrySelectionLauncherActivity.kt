a5d7f33c825ed6fe74da2f5dc9717b58952b1165
J-Jamet <jeremy.jamet@kunzisoft.com>
2022-04-14 19:11:33 +0200

    Fix unexpected lock of the app #1294

--- app/src/main/java/com/kunzisoft/keepass/activities/EntrySelectionLauncherActivity.kt
+++ app/src/main/java/com/kunzisoft/keepass/activities/EntrySelectionLauncherActivity.kt
@@ -205,7 +205,8 @@ class EntrySelectionLauncherActivity : DatabaseModeActivity() {
             }
             // New task needed because don't launch from an Activity context
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                    Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                    Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
             context.startActivity(intent)
         }
     }
