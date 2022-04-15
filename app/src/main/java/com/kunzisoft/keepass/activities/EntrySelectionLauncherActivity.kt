2a87eaf3e57e509776a87ff0b2220e45d8cd9b9e
J-Jamet <jeremy.jamet@kunzisoft.com>
2022-04-15 12:21:14 +0200

    Upgrade to 3.4.2 and fix service parameter and workflow

--- app/src/main/java/com/kunzisoft/keepass/activities/EntrySelectionLauncherActivity.kt
+++ app/src/main/java/com/kunzisoft/keepass/activities/EntrySelectionLauncherActivity.kt
@@ -205,8 +205,7 @@ class EntrySelectionLauncherActivity : DatabaseModeActivity() {
             }
             // New task needed because don't launch from an Activity context
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                    Intent.FLAG_ACTIVITY_CLEAR_TASK or
-                    Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK
             context.startActivity(intent)
         }
     }
