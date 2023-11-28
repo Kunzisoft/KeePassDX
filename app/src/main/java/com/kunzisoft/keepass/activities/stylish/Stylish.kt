1eb0eababb14cc188c75300cd4681fef04e41b7c
pruthvi-21 <bps.21@proton.me>
2023-11-28 15:10:21 +0530

    Removed Sun style

--- app/src/main/java/com/kunzisoft/keepass/activities/stylish/Stylish.kt
+++ app/src/main/java/com/kunzisoft/keepass/activities/stylish/Stylish.kt
@@ -77,7 +77,6 @@ object Stylish {
             context.getString(R.string.list_style_name_night) -> context.getString(R.string.list_style_name_light)
             context.getString(R.string.list_style_name_black) -> context.getString(R.string.list_style_name_white)
             context.getString(R.string.list_style_name_dark) -> context.getString(R.string.list_style_name_clear)
-            context.getString(R.string.list_style_name_red_night) -> context.getString(R.string.list_style_name_red)
             context.getString(R.string.list_style_name_reply_night) -> context.getString(R.string.list_style_name_reply)
             context.getString(R.string.list_style_name_purple_dark) -> context.getString(R.string.list_style_name_purple)
             else -> styleString
@@ -90,7 +89,6 @@ object Stylish {
             context.getString(R.string.list_style_name_light) -> context.getString(R.string.list_style_name_night)
             context.getString(R.string.list_style_name_white) -> context.getString(R.string.list_style_name_black)
             context.getString(R.string.list_style_name_clear) -> context.getString(R.string.list_style_name_dark)
-            context.getString(R.string.list_style_name_red) -> context.getString(R.string.list_style_name_red_night)
             context.getString(R.string.list_style_name_reply) -> context.getString(R.string.list_style_name_reply_night)
             context.getString(R.string.list_style_name_purple) -> context.getString(R.string.list_style_name_purple_dark)
             else -> styleString
@@ -129,8 +127,6 @@ object Stylish {
             context.getString(R.string.list_style_name_black) -> R.style.KeepassDXStyle_Black
             context.getString(R.string.list_style_name_clear) -> R.style.KeepassDXStyle_Clear
             context.getString(R.string.list_style_name_dark) -> R.style.KeepassDXStyle_Dark
-            context.getString(R.string.list_style_name_red) -> R.style.KeepassDXStyle_Red
-            context.getString(R.string.list_style_name_red_night) -> R.style.KeepassDXStyle_Red_Night
             context.getString(R.string.list_style_name_reply) -> R.style.KeepassDXStyle_Reply
             context.getString(R.string.list_style_name_reply_night) -> R.style.KeepassDXStyle_Reply_Night
             context.getString(R.string.list_style_name_purple) -> R.style.KeepassDXStyle_Purple
